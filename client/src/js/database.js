09f194a2c3c9d375bf0ca8aeb962e8ec456bcb26
Radhika <cu9rad@gmail.com>
2022-04-19 03:52:28 -0400

    database file logic implemented

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,32 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // TODO: Add logic to a method that accepts some content and adds it to the database
+  export const putDb = async (content) => {
+    console.log('PUT to database');
+      // Create a connection to the database database and version we want to use.
+    const jateDb = await openDB('jate', 1);
+    // Create a new transaction and specify the database and data privileges.
+    const tx = jateDb.transaction('jate', 'readwrite');
+    // Open up the desired object store.
+    const store = tx.objectStore('jate');
+    const req = store.put({ id: 1, value: content });
+    // Get confirmation of the request.
+    const res = await req;
+    console.log('putDb successfully implemented', res)
+  };
+  // TODO: Add logic for a method that gets all the content from the database
+  export const getDb = async () => {
+    console.log('get all content from databse');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    // const req = store.getAll();
+    const req = store.get(1);
+    const res = await req;
+    console.log('getDb implemented successfully', res)
+    return res?.value;  
+  };
 
 initdb();
